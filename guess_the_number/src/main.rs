d809c8f98e9f947126f31824e632570a51b16d8b
Sammi Turner <ms.sammi.turner@gmail.com>
2022-06-20 18:16:52 +0100

    Add files via upload

---
+++ guess_the_number/src/main.rs
@@ -0,0 +1,28 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    loop {
+        println!("\nGuess the number? ");
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess).expect("Failed to read line.");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("{}", "Too small."),
+            Ordering::Greater => println!("{}", "Too big."),
+            Ordering::Equal => {
+                println!("{}", "You win!\n");
+                break;
+            }
+        }
+    }
+}
+
